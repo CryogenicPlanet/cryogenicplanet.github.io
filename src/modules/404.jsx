ae3d44dff05aada4859de1c85d8682f28fe828c4
Rahul Tarak <rahultarak12345@gmail.com>
2020-04-19 20:48:03 +0530

    A22 Post Email

--- src/modules/404.jsx
+++ src/modules/404.jsx
@@ -8,7 +8,11 @@ class FourOFour extends Component {
         status="404"
         title="404"
         subTitle="Sorry, the page you visited does not exist."
-        extra={<Link to="/"><Button type="primary">Back Home</Button></Link>}
+        extra={
+          <Link to="/">
+            <Button type="primary">Back Home</Button>
+          </Link>
+        }
       />
     );
   }
