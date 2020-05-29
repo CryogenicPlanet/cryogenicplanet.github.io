b722c6caa728d4ed1474067cb665e87d97e644a5
Rahul Tarak <rahultarak12345@gmail.com>
2020-05-29 13:34:34 +0530

    Project Parallax Effect

--- src/modules/projects/projects.jsx
+++ src/modules/projects/projects.jsx
@@ -4,6 +4,7 @@ import React, { Component, Fragment } from "react";
 import data from "./data";
 import Image from "material-ui-image";
 import { Col, Row, List, Typography, Icon, Tooltip, Button } from "antd";
+import { Parallax, Background } from "react-parallax";
 //import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
@@ -12,11 +13,11 @@ class Projects extends Component {
     extraProject: false,
     fontSize: this.props.fontSize,
     screenWidth: this.props.screenWidth,
-    isMobile: this.props.isMobile
+    isMobile: this.props.isMobile,
   };
   moreProjects = () => {
     this.setState({
-      extraProject: true
+      extraProject: true,
     });
   };
   componentDidUpdate(prevProps) {
@@ -26,7 +27,7 @@ class Projects extends Component {
     ) {
       this.setState({
         screenWidth: this.props.screenWidth,
-        isMobile: this.props.isMobile
+        isMobile: this.props.isMobile,
       });
     }
   }
@@ -37,99 +38,29 @@ class Projects extends Component {
           <Row type="flex" justify="center" align="middle">
             <Title>Projects</Title>
           </Row>
-          <Row type="flex" justify="center" align="middle">
-            <Col>
-              <List
-                itemLayout="vertical"
-                size="large"
-                dataSource={data}
-                renderItem={item => (
-                  <Fragment>
-                    <div>
-                      {this.state.screenWidth < 769 ||
-                      this.state.isMobile === true ? (
-                        <Image
-                          src={item.image}
-                          onClick={() => console.log("onClick")}
-                          aspectRatio={16 / 9}
-                          disableSpinner
-                        />
-                      ) : null}
-                    </div>
-                    <List.Item
-                      key={item.title.name}
-                      actions={[
-                        <Row type="flex" justify="space-between" align="middle">
-                          <Fragment>
-                            <Col span={4}>
-                              <Tooltip title="Learn More">
-                                <a href={`#${item.id}`} disabled>
-                                  <Icon
-                                    type="more"
-                                    style={{ fontSize: this.state.fontSize }}
-                                    rotate="90"
-                                  />
-                                </a>
-                              </Tooltip>
-                            </Col>
-                            {item.links.map((link) => {
-                              return (
-                                <Col span={4}>
-                                  <Tooltip title={link.prompt}>
-                                    <a
-                                      href={link.href}
-                                      target="_blank"
-                                      rel="noopener noreferrer"
-                                    >
-                                      <Icon
-                                        type={link.type}
-                                        style={{
-                                          fontSize: this.state.fontSize
-                                        }}
-                                      />
-                                    </a>
-                                  </Tooltip>
-                                </Col>
-                              );
-                            })}
-                          </Fragment>
-                        </Row>
-                      ]}
-                      extra={
-                        this.state.screenWidth > 768 &&
-                        this.state.isMobile === false ? (
-                          <a href={item.links[0].href}>
-                            <img
-                              style={{ width: "13vw" }}
-                              alt="logo"
-                              src={item.image}
-                            />
-                          </a>
-                        ) : null
-                      }
-                    >
-                      <List.Item.Meta
-                        title={
-                          <a href={item.title.href}> {item.title.name} </a>
-                        }
-                        description={`${item.position} | From ${item.startDate} to ${item.endDate} in ${item.location}`}
-                      />
-                      {item.description.map((description) => {
-                        return <p>{description}</p>;
-                      })}
-                      {item.languages != null ? (
-                        <p>
-                          <b>{`Languages used ${item.languages}`}</b>
-                        </p>
-                      ) : (
-                        <p></p>
-                      )}
-                    </List.Item>
-                  </Fragment>
-                )}
-              ></List>
-            </Col>
-          </Row>
+
+          {data.map((project) => {
+            return (
+              <Fragment>
+                <Parallax
+                  bgImage={
+                    process.env.PUBLIC_URL +
+                    `/images/projects/${project.parallax}`
+                  }
+                  style={{ height: "25vw", paddingBottom: "5%" }}
+                  blur={{ min: -15, max: 15 }}
+                  bgImageStyle={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    overflow: "hidden",
+                  }}
+                  strength={150}
+                ></Parallax>
+              </Fragment>
+            );
+          })}
+
           <Row type="flex" justify="center" align="middle">
             <Tooltip title="Under Construction">
               <Button type="dashed" block disabled onClick={this.moreProjects}>
